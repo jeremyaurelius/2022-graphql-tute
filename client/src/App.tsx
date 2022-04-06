b54c94e4185c4eac463bf26fdde491e9d611ff4a
Jeremy <mail.jeremy.li@gmail.com>
2022-04-06 17:57:37 +1000

    added react client

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import BookList from './components/BookList';
+
+function App() {
+  return (
+    <div className="main">
+      <h1>Reading List</h1>
+      <BookList></BookList>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/BookList.tsx
@@ -0,0 +1,11 @@
+import './BookList.css';
+
+export default function BookList() {
+  return (
+    <div>
+      <ul className="book-list">
+        <li>Book name</li>
+      </ul>
+    </div>
+  );
+}
